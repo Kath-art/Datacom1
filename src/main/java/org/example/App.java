3d7ed86ca8c95dd1ba067da59dc16e1d89a20315
Kath <triggskatherine@gmail.com>
2025-09-15 09:34:53 +1200

    Reinstate main.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,6 @@
 package org.example;
 
-
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.Playwright;
-
-//public class App {
-//    public static void main(String[] args) {
-//        try (Playwright playwright = Playwright.create()) {
-//            Browser browser = playwright.chromium().launch();
-//    }
-//}
+public class App {
+    public static void main(String[] args) {
+    }
+}
